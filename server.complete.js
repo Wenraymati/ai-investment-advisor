0bc61828e355698c1b4a7402fb1d59c336032ba5
Wenraymati <wenraymati@users.noreply.github.com>
2025-10-16 14:42:21 -0300

    DELETE server.complete.js and cleanup - Force Railway to use server.nodb.js

--- server.complete.js
+++
@@ -1,5 +0,0 @@
-// Railway Start Command Redirect
-// Este archivo es un wrapper que ejecuta server.nodb.js
-// Railway tiene configurado `node server.complete.js` y no podemos cambiarlo desde la API
-
-require('./server.nodb.js');
